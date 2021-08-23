77aa2684fbba1a7844ef9218f6f8a88a04e9190f
Joshita <joshita.atmakuri@gmail.com>
2021-08-23 17:21:11 +0530

    Second commit- completed the navigation drawer.

--- app/src/main/java/com/example/smartclassroom/departments.java
+++ app/src/main/java/com/example/smartclassroom/departments.java
@@ -1,29 +1,80 @@
 package com.example.smartclassroom;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class departments extends AppCompatActivity {
-    private Button logout;
+public class departments extends AppCompatActivity  {
+    //private Button logout;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_departments);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.navigationView);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.menu_open,R.string.close_menu);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        logout=(Button)findViewById(R.id.logout);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(departments.this,MainActivity.class));
-            }
+
+
+
+                navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch(menuItem.getItemId()){
+                        case R.id.nav_CSE:
+                            Toast.makeText(departments.this,"CSE department has been selected",Toast.LENGTH_LONG).show();
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            break;
+                        case R.id.nav_ISE:
+                            Toast.makeText(departments.this,"ISE department has been selected",Toast.LENGTH_LONG).show();
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            break;
+                        case R.id.nav_ECE:
+                            Toast.makeText(departments.this,"ECE department has been selected",Toast.LENGTH_LONG).show();
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            break;
+                        case R.id.nav_signout:
+                            FirebaseAuth.getInstance().signOut();
+                            startActivity(new Intent(departments.this, MainActivity.class));
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            break;
+                    }
+                    return true;
+                }
+
+
         });
     }
+
 }
+
